386f921b5f3686bf0cc81b79539b0af247d6263f
Elvin Kukuh Nurdiyansah <elvinkukuh20@student.uns.ac.id>
2022-11-04 17:24:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RPL_Praktik
+ 
---
+++ app/src/main/java/com/example/rpl_prak/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.rpl_prak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View _bg__list_member_ek2;
+    private ImageView vector;
+    private TextView my_teams_ek6;
+    private View line_1_ek6;
+    private View line_2_ek6;
+    private View rectangle_1_ek6;
+    private ImageView ellipse_11_ek6;
+    private ImageView ellipse_7_ek6;
+    private View rectangle_2_ek6;
+    private ImageView rectangle_3;
+    private TextView d_3_teknik_informatika_ek6;
+    private TextView universitas_sebelas_maret_ek6;
+    private TextView elvin_kukuh_n_ek1;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek6;
+    private TextView project_manager_ek6;
+    private View rectangle_2_ek7;
+    private ImageView rectangle_3_ek1;
+    private TextView d_3_teknik_informatika_ek7;
+    private TextView universitas_sebelas_maret_ek7;
+    private TextView irvan_alvian_adi_n_ek1;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek7;
+    private TextView project_manager_ek7;
+    private View rectangle_2_ek8;
+    private ImageView rectangle_3_ek2;
+    private TextView d_3_teknik_informatika_ek8;
+    private TextView universitas_sebelas_maret_ek8;
+    private TextView farhan_arif_s_ek1;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek8;
+    private TextView project_manager_ek8;
+    private View rectangle_2_ek9;
+    private ImageView rectangle_3_ek3;
+    private TextView d_3_teknik_informatika_ek9;
+    private TextView universitas_sebelas_maret_ek9;
+    private TextView mohammad_jose_r_ek1;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek9;
+    private TextView project_manager_ek9;
+    private View rectangle_2_ek10;
+    private ImageView rectangle_3_ek4;
+    private TextView d_3_teknik_informatika_ek10;
+    private TextView universitas_sebelas_maret_ek10;
+    private TextView imam_abdilah_p_ek1;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek10;
+    private TextView project_manager_ek10;
+    private View rectangle_2_ek11;
+    private ImageView rectangle_3_ek5;
+    private TextView d_3_teknik_informatika_ek11;
+    private TextView universitas_sebelas_maret_ek11;
+    private TextView muhamad_faris_rafi;
+    private ImageView whatsapp_image_2022_07_26_at_10_13_1_ek11;
+    private TextView project_manager_ek11;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _bg__list_member_ek2 = (View) findViewById(R.id._bg__list_member_ek2);
+        vector = (ImageView) findViewById(R.id.vector);
+        my_teams_ek6 = (TextView) findViewById(R.id.my_teams_ek6);
+        line_1_ek6 = (View) findViewById(R.id.line_1_ek6);
+        line_2_ek6 = (View) findViewById(R.id.line_2_ek6);
+        rectangle_1_ek6 = (View) findViewById(R.id.rectangle_1_ek6);
+        ellipse_11_ek6 = (ImageView) findViewById(R.id.ellipse_11_ek6);
+        ellipse_7_ek6 = (ImageView) findViewById(R.id.ellipse_7_ek6);
+        rectangle_2_ek6 = (View) findViewById(R.id.rectangle_2_ek6);
+        rectangle_3 = (ImageView) findViewById(R.id.rectangle_3);
+        d_3_teknik_informatika_ek6 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek6);
+        universitas_sebelas_maret_ek6 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek6);
+        elvin_kukuh_n_ek1 = (TextView) findViewById(R.id.elvin_kukuh_n_ek1);
+        whatsapp_image_2022_07_26_at_10_13_1_ek6 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek6);
+        project_manager_ek6 = (TextView) findViewById(R.id.project_manager_ek6);
+        rectangle_2_ek7 = (View) findViewById(R.id.rectangle_2_ek7);
+        rectangle_3_ek1 = (ImageView) findViewById(R.id.rectangle_3_ek1);
+        d_3_teknik_informatika_ek7 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek7);
+        universitas_sebelas_maret_ek7 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek7);
+        irvan_alvian_adi_n_ek1 = (TextView) findViewById(R.id.irvan_alvian_adi_n_ek1);
+        whatsapp_image_2022_07_26_at_10_13_1_ek7 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek7);
+        project_manager_ek7 = (TextView) findViewById(R.id.project_manager_ek7);
+        rectangle_2_ek8 = (View) findViewById(R.id.rectangle_2_ek8);
+        rectangle_3_ek2 = (ImageView) findViewById(R.id.rectangle_3_ek2);
+        d_3_teknik_informatika_ek8 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek8);
+        universitas_sebelas_maret_ek8 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek8);
+        farhan_arif_s_ek1 = (TextView) findViewById(R.id.farhan_arif_s_ek1);
+        whatsapp_image_2022_07_26_at_10_13_1_ek8 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek8);
+        project_manager_ek8 = (TextView) findViewById(R.id.project_manager_ek8);
+        rectangle_2_ek9 = (View) findViewById(R.id.rectangle_2_ek9);
+        rectangle_3_ek3 = (ImageView) findViewById(R.id.rectangle_3_ek3);
+        d_3_teknik_informatika_ek9 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek9);
+        universitas_sebelas_maret_ek9 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek9);
+        mohammad_jose_r_ek1 = (TextView) findViewById(R.id.mohammad_jose_r_ek1);
+        whatsapp_image_2022_07_26_at_10_13_1_ek9 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek9);
+        project_manager_ek9 = (TextView) findViewById(R.id.project_manager_ek9);
+        rectangle_2_ek10 = (View) findViewById(R.id.rectangle_2_ek10);
+        rectangle_3_ek4 = (ImageView) findViewById(R.id.rectangle_3_ek4);
+        d_3_teknik_informatika_ek10 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek10);
+        universitas_sebelas_maret_ek10 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek10);
+        imam_abdilah_p_ek1 = (TextView) findViewById(R.id.imam_abdilah_p_ek1);
+        whatsapp_image_2022_07_26_at_10_13_1_ek10 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek10);
+        project_manager_ek10 = (TextView) findViewById(R.id.project_manager_ek10);
+        rectangle_2_ek11 = (View) findViewById(R.id.rectangle_2_ek11);
+        rectangle_3_ek5 = (ImageView) findViewById(R.id.rectangle_3_ek5);
+        d_3_teknik_informatika_ek11 = (TextView) findViewById(R.id.d_3_teknik_informatika_ek11);
+        universitas_sebelas_maret_ek11 = (TextView) findViewById(R.id.universitas_sebelas_maret_ek11);
+        muhamad_faris_rafi = (TextView) findViewById(R.id.muhamad_faris_rafi);
+        whatsapp_image_2022_07_26_at_10_13_1_ek11 = (ImageView) findViewById(R.id.whatsapp_image_2022_07_26_at_10_13_1_ek11);
+        project_manager_ek11 = (TextView) findViewById(R.id.project_manager_ek11);
+    }
+}
